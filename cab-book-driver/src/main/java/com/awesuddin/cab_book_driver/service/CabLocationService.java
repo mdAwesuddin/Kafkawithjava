1e4cbf30bdcfbb429e16cb9eaf002b3a67965ff1
mdAwesuddin <mdawesuddin@gmail.com>
2025-06-14 23:16:03 +0530

    Fix: Add cab-book-driver as a normal folder

---
+++ cab-book-driver/src/main/java/com/awesuddin/cab_book_driver/service/CabLocationService.java
@@ -0,0 +1,18 @@
+package com.awesuddin.cab_book_driver.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CabLocationService {
+
+    @Autowired
+    private KafkaTemplate<String,Object> kafkaTemplate;
+
+    public boolean updateLocation(String location) {
+        kafkaTemplate.send("cab-location", location);
+        return true;
+    }
+
+}
